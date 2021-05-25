0762a22eeb761bae982083f3f74017c35d909d69
superstones <292454993@qq.com>
2021-05-25 20:06:34 +0800

    first commit

---
+++ week1/Chapter 1&2/Hello Python!/Hello World.py
@@ -0,0 +1,8 @@
+print("Hello Python interpreter!")
+print("Hello World!")
+message="Hello Python World!"
+print(message)
+message="Hello Python Crash Course world!"
+print(message)
+mesage="Hello Stone!"
+print(mesage)
---
+++ week1/Chapter 1&2/String Exercise/name.py
@@ -0,0 +1,5 @@
+name="ada love lace"
+print(name.title())#首字母大写
+name="Ada Love Lacce"
+print(name.upper())#全部大写
+print(name.lower())#全部小写
