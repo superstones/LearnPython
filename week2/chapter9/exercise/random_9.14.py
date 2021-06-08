19e7e50ab5e2ae2feff9a4bbd5c296e2f02d4ce2
superstones <292454993@qq.com>
2021-06-08 19:42:10 +0800

    第九章学习完成

--- week2/chapter9/exercise/random_9.14.py
+++ week2/chapter9/exercise/random_9.14.py
@@ -5,18 +5,30 @@ class Die():
     def __init__(self, sides=6):
         self.sides = sides
 
-    def roll_die(self, times):
-        self.times = times
-        for x in range(times):
-            x = randint(1, self.sides)
-            print(x)
+    def roll_die(self):
+        return randint(1, self.sides)
 
 
-my_die = Die()
-my_die.roll_die(10)
-print("\n")
-ten_die = Die(10)
-ten_die.roll_die(10)
-print("\n")
-twenty_die = Die(20)
-twenty_die.roll_die(10)
+d6 = Die()
+results = []
+for roll_num in range(10):
+    result = d6.roll_die()
+    results.append(result)
+print("10 rolls of a 6-sided die:")
+print(results)
+
+d10 = Die(sides=10)
+results = []
+for roll_num in range(10):
+    result = d10.roll_die()
+    results.append(result)
+print("\n10 rolls of a 10-sided die:")
+print(results)
+
+d20 = Die(sides=20)
+results = []
+for roll_num in range(10):
+    result = d10.roll_die()
+    results.append(result)
+print("\n10 rolls of a 20-sided die:")
+print(results)
